79257091252aca56bfc6478553a19773b700a04b
xQudo <alyaqdhan690s@gmail.com>
2026-08-03 23:21:12 +0400

    User & TrainerProfile models

---
+++ GFCM/Models/TrainerProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace GFCM.Models
+{
+    public class TrainerProfile
+    {
+        [Key]
+        [JsonIgnore]
+        public int trainerProfileId { get; set; }
+
+
+        [ForeignKey("user")]
+        public int userId { get; set; }
+        [JsonIgnore]
+        public User user { get; set; }
+
+
+        [ForeignKey("branch")]
+        public int branchId { get; set; }
+        [JsonIgnore]
+        public Branch branch { get; set; }
+
+
+        public string specialization { get; set; }
+        public string bio { get; set; }
+        public int yearsOfExperience { get; set; }
+        public string certificationDetails { get; set; }
+    }
+}
