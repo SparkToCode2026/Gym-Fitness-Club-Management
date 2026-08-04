af059f570ca7db2e9cd823e27853494d02de5905
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-04 14:59:21 +0400

    add attributes with there relationships and all requirment

--- GFCM/Models/Equipment.cs
+++ GFCM/Models/Equipment.cs
@@ -1,7 +1,32 @@
-﻿namespace GFCM.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GFCM.Models
 {
     public class Equipment
     {
+        [Key]
+        public int EquipmentId { get; set; }
+        public string EquipmentName { get; set; }
+        public int EquipmentQuantity { get; set; }
+        public DateTime EquipmentPurchaseDate { get; set; }
+        public enum EquipmentMaintenanceStatus
+        {
+            Available,
+            InUse,
+            UnderMaintenance,
+            OutOfService
+        }
+
+        //[1] Branch : [M] Equipment
+        [ForeignKey("_branch")]
+        public int BranchId { get; set; }
+
+        //navigation
+        //? helps on response purpus
+        public Branch? _branch { get; set; } 
+
+
 
     }
 }
--- GFCM/Models/Payment.cs
+++ GFCM/Models/Payment.cs
@@ -1,6 +1,39 @@
-﻿namespace GFCM.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GFCM.Models
 {
     public class Payment
     {
+        [Key]
+        public int PaymentId { get; set; }
+        public double PaymentAmount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public enum PaymentMethod
+        {
+            Cash,
+            CreditCard,
+            DebitCard,
+            BankTransfer,
+        }
+        public enum PaymentStatus
+        {
+            Pending,
+            Paid,
+            Failed,
+            Refunded
+        }
+
+        //[1] User : [M] Payment
+        [ForeignKey("_user")]
+        public int UserId { get; set; }
+
+        //[1] Membership : [M] Payment
+        [ForeignKey("_membership")]
+        public int MembershipId { get; set; }
+
+
+        public User? _user {  get; set; }
+        public Membership? _membership {  get; set; }
     }
 }
