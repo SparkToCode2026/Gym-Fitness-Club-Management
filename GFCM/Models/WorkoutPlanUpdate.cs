87547d547536466ee23236bea43fcb596a10c655
xQudo <alyaqdhan690s@gmail.com>
2026-08-06 23:24:41 +0400

    workoutplan controller

---
+++ GFCM/Models/WorkoutPlanUpdate.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GFCM.Models
+{
+    public class WorkoutPlanUpdate
+    {
+        [Required]
+        public string planTitle { get; set; }
+        public string planDescription { get; set; }
+        public DateTime? endDate { get; set; }
+    }
+}
