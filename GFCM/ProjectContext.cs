874b76c63553b70b0404794d286a6e948c50657a
xQudo <58079015+Alyaqdhans@users.noreply.github.com>
2026-08-08 02:28:44 +0400

    Remove Equipment object from ProjectContext
    Removed internal Equipment object from ProjectContext.

--- GFCM/ProjectContext.cs
+++ GFCM/ProjectContext.cs
@@ -5,8 +5,6 @@ namespace GFCM
 {
     public class ProjectContext : DbContext
     {
-        internal object Equipment;
-
         // Dev1:
         public DbSet<User> users {  get; set; }
         public DbSet<TrainerProfile> trainerProfiles { get; set; }
