29c1cd739efc507b10ed5c90dfccdcdb54a74d53
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-12 11:19:57 +0400

    load attendance

--- GFCM/wwwroot/js/attendance.js
+++ GFCM/wwwroot/js/attendance.js
@@ -1,1 +1,14 @@
-﻿
+﻿requireAuth();
+
+let attendanceRecords = [];
+let elapsedTimer = null;
+
+//load attendance
+document.addEventListener("DOMContentLoaded", async () => {
+    await loadAttendance();
+
+    // Updating "In gym" durations every minute
+    elapsedTimer = setInterval(() => {
+        updateLiveDurations();
+    }, 60000);
+});
