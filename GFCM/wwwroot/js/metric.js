04a0c9acf11f60fe7b79bf1099170fa9d8166993
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-13 22:33:57 +0400

    date formate

--- GFCM/wwwroot/js/metric.js
+++ GFCM/wwwroot/js/metric.js
@@ -1981,3 +1981,28 @@ function clearSummary() {
 
 }
 
+function formatDate(value) {
+
+    if (!value) {
+        return "-";
+    }
+
+
+    const date =
+        new Date(value);
+
+
+    if (
+        Number.isNaN(
+            date.getTime()
+        )
+    ) {
+
+        return value;
+
+    }
+
+
+    return date.toLocaleDateString();
+
+}
