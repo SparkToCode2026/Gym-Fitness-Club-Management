ba6de360d53c3e29e32768a7ae4598cd03735e0a
Badour Alkalbani <bed20oor01suliman@gmail.com>
2026-08-15 03:48:20 +0400

    loading page for member, trainer, workoutplan

--- GFCM/wwwroot/js/workout.js
+++ GFCM/wwwroot/js/workout.js
@@ -1,1 +1,44 @@
-﻿
+﻿requireAuth();
+
+let workoutPlans = [];
+let members = [];
+let trainers = [];
+
+let editModal;
+let trainerModal;
+let deleteModal;
+
+//loading page for member, trainer, workoutplan
+document.addEventListener("DOMContentLoaded", async () => {
+    editModal = new bootstrap.Modal(
+        document.getElementById("editModal")
+    );
+    trainerModal = new bootstrap.Modal(
+        document.getElementById("trainerModal")
+    );
+    deleteModal = new bootstrap.Modal(
+        document.getElementById("deleteModal")
+    );
+    document
+        .getElementById("filterUserId")
+        .addEventListener("change", loadWorkoutPlans);
+    document
+        .getElementById("activeToggle")
+        .addEventListener("change", loadWorkoutPlans);
+    document
+        .getElementById("addWorkoutForm")
+        .addEventListener("submit", createWorkoutPlan);
+    document
+        .getElementById("editWorkoutForm")
+        .addEventListener("submit", updateWorkoutPlan);
+    document
+        .getElementById("trainerForm")
+        .addEventListener("submit", reassignTrainer);
+    document
+        .getElementById("confirmDeleteBtn")
+        .addEventListener("click", deleteWorkoutPlan);
+    await loadMemberOptions();
+    await loadTrainerOptions();
+    await loadWorkoutPlans();
+
+});
